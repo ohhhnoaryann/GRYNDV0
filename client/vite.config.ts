957e10e05c19c51322fc48c25ac24b2bdaa5af15
Aryan Pareek <aryannpareek380@gmail.com>
2025-07-11 23:12:10 +0530

    fix: move index.html to root and update vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,14 +4,16 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  root: "client",
   plugins: [
     react(),
     runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" && process.env.REPL_ID === undefined
+      ? [require("@replit/vite-plugin-cartographer")()]
+      : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "src"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
